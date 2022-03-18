a773197c3189d330c0cead6ddb4cbff4ed05529e
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-18 14:21:43 +0900

    [Feature]  - Automatic Generated codes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<template></template>
+<script>
+export default
+{
+  name: '', 
+  components: {}, 
+  data() {
+  return {
+      sampleData: ''
+    };
+  },
+  beforeCreate() {}, 
+  created() {},
+
+  beforeMount() {}, 
+  mounted() {}, 
+  beforeUpdate() {}, 
+  updated() {}, 
+  beforeUnmount() {}, 
+  unmounted() {}, 
+  methods() {}
+}
+
+</script>
