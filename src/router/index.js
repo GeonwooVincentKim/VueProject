47c8b29822c600eca65391adee307d897f369c18
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-18 16:23:28 +0900

    [Feature] - Solved access to DataBinding issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DataBinding from "../views/DataBinding.vue";
 
 const routes = [
   {
     // Path -> Home
     // Name -> Home
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
     // Path -> About
     // Name -> About
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: DataBinding
   }
 ]
 
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,24 +1,5 @@
-<template></template>
-<script>
-export default
-{
-  name: '', 
-  components: {}, 
-  data() {
-  return {
-      sampleData: ''
-    };
-  },
-  beforeCreate() {}, 
-  created() {},
-
-  beforeMount() {}, 
-  mounted() {}, 
-  beforeUpdate() {}, 
-  updated() {}, 
-  beforeUnmount() {}, 
-  unmounted() {}, 
-  methods() {}
-}
-
-</script>
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
