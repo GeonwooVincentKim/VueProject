b994375d59e24868e5f1c529635bed32e5b30ddc
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-29 13:04:54 +0900

    [Update] - Updated App.vue file

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <!-- <router-link to="/about">About</router-link> | -->
-    <router-link to="/databinding">Data Binding</router-link> | 
+    <router-link to="/databinding">Data Binding</router-link>
+    
+    <!-- <router-link to="/databinding">Data Binding</router-link> | 
     <router-link to="/databindinghtml">Data Binding HTML</router-link> | 
     <router-link to="/databindinginputtext">Data Binding Input Text</router-link> | 
     <router-link to="/dataBindingInputNumber">Data Binding Input Number</router-link> | 
@@ -10,7 +12,7 @@
     <router-link to="/dataBindingSelect">Data Binding Select</router-link> | 
     <router-link to="./dataBindingCheckbox">Data Binding Check Box</router-link> | 
     <router-link to="./dataBindingCheckbox2">Data Binding Check Box 2</router-link> | 
-    <router-link to="./dataBindingAttribute">Data Binding Attribute</router-link>
+    <router-link to="./dataBindingAttribute">Data Binding Attribute</router-link> -->
   </nav>
   <router-view/>
 </template>
