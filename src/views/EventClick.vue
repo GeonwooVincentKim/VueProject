887fb06ffbf414dad544e1a56ef9832a9d2b3c43
GeonwooVincentKim <kdsnop@gmail.com>
2022-03-30 17:55:34 +0900

    [Update] - Draw the bottom-Line

--- src/views/EventClick.vue
+++ src/views/EventClick.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
+    <div id="topButton">
         <button type="button" @click="increaseCounter">Add 1</button>
         <p>The Counter is : {{ counter }}</p>
+    </div>
+    <div id="bottomButton">
+        <button type="button" @click="one(), two()">CLick</button>
     </div>
 </template>
 <script>
@@ -14,7 +17,22 @@ export default {
     methods: {
         increaseCounter() {
             this.counter = this.counter + 1;
+        },
+        
+        one() {
+            alert("One");
+        },
+
+        two() {
+            alert("Two");
         }
     }
 }
 </script>
+<style scoped>
+    div {padding: 10px 0; line-height: 5px;}
+    div p {font-size: 15px; line-height: 22px;}
+
+    #topButton {border-bottom: 1px solid #ebebeb;}
+    div#bottomButton {padding: 15px 0 15px 0px}
+</style>
