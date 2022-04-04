a9c4550a6cfb22363ea6635036ea3a5bcb0aaa1c
GeonwooVincentKim <kdsnop@gmail.com>
2022-04-04 09:26:38 +0900

    [Update] - Applied `page-title` instead of `PageTitle`

--- src/views/NestedComponent.vue
+++ src/views/NestedComponent.vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
-        <PageTitle title="부모 컴포넌트에서 자식 컴포넌트로 데이터 전달" />
-    </div>
+    <page-title title="title" />
 </template>
 
 <script>
     import PageTitle from '../components/PageTitle';
 
     export default {
-        components: { PageTitle }
+        components: { PageTitle },
+        data() {
+            return {
+                title: "동적 페이지 타이틀"
+            }
+        }
     }
 </script>
